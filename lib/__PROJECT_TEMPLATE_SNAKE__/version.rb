notCalle/glistening_ruby

Setup basic Ruby Gem project structure
This creates a project structure for a Ruby Gem, with
`bundler`, `cucumber`, `rubocop` and `rspec` already setup.

lib/glistening_ruby/version.rb
@@ -1,0 +1,10 @@
+# frozen_string_literal: true
+
+module GlisteningRuby # rubocop:disable Style/Documentation
+  begin
+    require 'git-version-bump'
+    VERSION = GVB.version.freeze
+  rescue LoadError
+    VERSION = '0.0.0.UNDEF'
+  end
+end

spec/spec_helper.rb
@@ -1,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'bundler/setup'
+require 'glistening_ruby'
+
+RSpec.configure do |config|
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = '.rspec_status'
+
+  # Disable RSpec exposing methods globally on `Module` and `main`
+  config.disable_monkey_patching!
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end

notCalle/project-template

lib/__PROJECT_TEMPLATE_SNAKE__/version.rb
@@ -1,0 +1,10 @@
+# frozen_string_literal: true
+
+module __PROJECT_TEMPLATE_CAMEL__ # rubocop:disable Style/Documentation
+  begin
+    require 'git-version-bump'
+    VERSION = GVB.version.freeze
+  rescue LoadError
+    VERSION = '0.0.0.UNDEF'
+  end
+end

spec/spec_helper.rb
@@ -1,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'bundler/setup'
+require '__PROJECT_TEMPLATE_SNAKE__'
+
+RSpec.configure do |config|
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = '.rspec_status'
+
+  # Disable RSpec exposing methods globally on `Module` and `main`
+  config.disable_monkey_patching!
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end